a507b82e0fe91adf8b342f7f2b2834386def282c
Niko Petruhin <Niko00046@ya.ru>
2021-12-01 11:44:43 +0300

    Добавьте файлы проекта.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = Singleton.GetInstance("Уникальный экземпляр");
+            var singleton2 = Singleton.GetInstance("Не уникальный экземпляр");
+            //В консоль будет выведено "Уникальный экземпляр", так как инициализация нового экземпляра не произошла
+            Console.WriteLine(singleton2.Data);
+            
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Одиночка работает, обе переменные содержат один и тот же экземпляр.");
+            }
+            else
+            {
+                Console.WriteLine("Одиночка не работает, переменные содержат разные экземпляры.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// Класс, реализующий паттерн Одиночка, гарантирующий, что для класса будет создан только один единственный экземпляр.
+    /// </summary>
+    public class Singleton
+    {
+        /// <summary>
+        /// Основной объект, в котором будет храниться уникальный экземпляр класса. 
+        /// </summary>
+        private static Singleton _instance;
+
+        /// <summary>
+        /// Какие-либо хранимые данные.
+        /// </summary>
+        private string _data;
+
+        /// <summary>
+        /// Данные, используемые в классе.
+        /// </summary>
+        public string Data => _data;
+
+        /// <summary>
+        /// Защищенный конструктор для инициализации единственного экземпляра класса.
+        /// </summary>
+        /// <param name="data">Данные, используемые в классе.</param>
+        private Singleton(string data)
+        {
+            _data = data;
+        }
+
+        /// <summary>
+        /// Получить экземпляр класса.
+        /// </summary>
+        /// <param name="data">Инициализирующие данные класса.</param>
+        /// <returns>Уникальный экземпляр класса.</returns>
+        public static Singleton GetInstance(string data)
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton(data);
+            }
+            return _instance;
+        }
+    }
+}
