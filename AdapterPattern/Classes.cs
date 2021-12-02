149def550b3094f887348ea3b8834abc273ed541
Niko Petruhin <Niko00046@ya.ru>
2021-12-02 11:27:27 +0300

    Реализация паттерна Адаптер

---
+++ AdapterPattern/Classes.cs
@@ -0,0 +1,48 @@
+﻿namespace AdapterPattern
+{
+    /// <summary>
+    /// Интерфейс по которому работает адаптер.
+    /// </summary>
+    public interface ITarget
+    {
+        /// <summary>
+        /// Работа адаптера.
+        /// </summary>
+        string GetRequest();
+    }
+
+    /// <summary>
+    /// Изначальный класс, к которому нужно применить Адаптер.
+    /// </summary>
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Специфическая функциональность";
+        }
+    }
+
+    /// <summary>
+    /// Адаптер.
+    /// </summary>
+    class Adapter : ITarget
+    {
+        /// <summary>
+        /// Адаптируемый класс.
+        /// </summary>
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        /// <summary>
+        /// Работа адаптера.
+        /// </summary>
+        public string GetRequest()
+        {
+            return $"Адаптер работает и использует '{_adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine(adaptee.GetSpecificRequest());
+            Console.WriteLine(target.GetRequest());
+
+            Console.ReadLine();
+        }
+    }
+}
